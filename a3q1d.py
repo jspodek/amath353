48e2f4e05ad63feb71c787724ae6283e44890766
jspodek <73512558+jspodek@users.noreply.github.com>
2023-02-27 09:39:23 -0500

    Changed lambda

--- a3q1d.py
+++ a3q1d.py
@@ -16,7 +16,7 @@ def f1(x):
 def f2(x):
     return 2*x / (x**2 - 1)
 
-x_vals = np.linspace(0,10,1000)
+x_vals = np.linspace(0,30,1000)
 
 y1 = f1(x_vals)
 y2 = f2(x_vals)
@@ -24,11 +24,12 @@ y2 = f2(x_vals)
 intersection_points = np.argwhere(np.diff(np.sign(y1 - y2))).flatten()
 intersection_c_vals = np.array([x_vals[i] for i in intersection_points])
 
-print("[", end="")
+lambda_vals = []
+
 for i in range(len(y1)):
     if my_in(y1[i], y2):
-        print(x_vals[i], end="")
-        print(", ", end="")
-else:
-    print("]")
+        lambda_vals.append(np.sqrt(x_vals[i]))
+
+
+print(lambda_vals)
 
