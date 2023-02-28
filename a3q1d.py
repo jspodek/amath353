c3f5e7f3dd9e5ba0f814bb370f088df81aa7192f
jspodek <73512558+jspodek@users.noreply.github.com>
2023-02-28 10:09:11 -0500

    Update a3q1d.py

--- a3q1d.py
+++ a3q1d.py
@@ -12,23 +12,25 @@ def my_in(a,b):
     return False
 
 def f1(x):
-    return np.tan(x) 
+    return np.tan(np.sqrt(x)) 
 def f2(x):
-    return 2*x / (x**2 - 1)
+    return 2*np.sqrt(x) / (x - 1)
 
-x_vals = np.linspace(0,30,1000)
+x_vals = np.linspace(-100,100,100000)
 
 y1 = f1(x_vals)
 y2 = f2(x_vals)
 
-intersection_points = np.argwhere(np.diff(np.sign(y1 - y2))).flatten()
-intersection_c_vals = np.array([x_vals[i] for i in intersection_points])
+plt.plot(x_vals, y1, label="tan")
+plt.plot(x_vals, y2, label="rational")
+
+plt.show()
 
 lambda_vals = []
 
 for i in range(len(y1)):
     if my_in(y1[i], y2):
-        lambda_vals.append(np.sqrt(x_vals[i]))
+        lambda_vals.append(x_vals[i])
 
 
 print(lambda_vals)
